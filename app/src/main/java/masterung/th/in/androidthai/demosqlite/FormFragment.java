4f0d94ccec514dcca5790adb4ffb147cbd03e097
masterUNG <phrombutr@gmail.com>
2018-12-26 15:58:20 +0700

    Finish 26dec

--- app/src/main/java/masterung/th/in/androidthai/demosqlite/FormFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/demosqlite/FormFragment.java
@@ -2,10 +2,18 @@ package masterung.th.in.androidthai.demosqlite;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,11 +21,92 @@ import android.view.ViewGroup;
  */
 public class FormFragment extends Fragment {
 
+    //    Explicit
+    private String nameString, surnameString, genderString,
+            ageString, allergicString, detailString;
 
     public FormFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Gender Controller
+        genderController();
+
+//        Save Controller
+        saveController();
+
+//        Age Controller
+        ageController();
+
+    }   // Main Method
+
+    private void ageController() {
+
+        String[] strings = new String[]{"0 - 10", "11 - 20", "21 - 30", "31 - 40", "41 - 50", "Over 51"};
+        Spinner spinner = getView().findViewById(R.id.spnAge);
+
+
+
+    }
+
+    private void genderController() {
+        RadioGroup radioGroup = getView().findViewById(R.id.ragGender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+
+                switch (checkedId) {
+                    case R.id.radMale:
+                        genderString = "0";
+                        break;
+                    case R.id.radFemale:
+                        genderString = "1";
+                        break;
+
+                }
+
+            }
+        });
+    }
+
+    private void saveController() {
+        Button button = getView().findViewById(R.id.btnSave);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Get Value From Edittext
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText surnameEditText = getView().findViewById(R.id.edtSurname);
+                EditText detailEditText = getView().findViewById(R.id.edtDetail);
+
+                nameString = nameEditText.getText().toString().trim();
+                surnameString = surnameEditText.getText().toString().trim();
+                detailString = detailEditText.getText().toString().trim();
+
+                MyAlert myAlert = new MyAlert(getActivity());
+
+                RadioButton maleRadioButton = getView().findViewById(R.id.radMale);
+                RadioButton femaleRadioButton = getView().findViewById(R.id.radFemale);
+
+//                Check Space
+                if (nameString.isEmpty() || surnameString.isEmpty() || detailString.isEmpty()) {
+//                    Have Space
+                    myAlert.normalDialog("Have Space", "Please Fill Every Blank");
+                } else if (!(maleRadioButton.isChecked() || femaleRadioButton.isChecked())) {
+//                        Non Choose Gender
+                    myAlert.normalDialog("Non Gender", "Please Choose Male or Female");
+                }
+
+
+            }   // onClick
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
