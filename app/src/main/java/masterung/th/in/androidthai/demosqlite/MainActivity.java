13d3d6c628dd2306c6221462960b0a69005995c8
masterUNG <phrombutr@gmail.com>
2018-12-26 11:50:27 +0700

    Toolbar Success

--- app/src/main/java/masterung/th/in/androidthai/demosqlite/MainActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/demosqlite/MainActivity.java
@@ -2,6 +2,7 @@ package masterung.th.in.androidthai.demosqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.title_toolbar));
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_action_hambager);
     }
-}
+
+}   // Main Class
